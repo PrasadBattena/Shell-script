9cc91bcafbea6128fcae81bfbf58ae641a226850
prasad <prasadbattena@gmail.com>
2025-02-09 12:26:47 +0530

    converstaion added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "rahim:: hi robert"
-echo "robert:: hello rahim"
-echo "rahim:: how are you doing?"
-echo "robert:: i am good. how are you?"
+echo "$person1:: hi robert"
+echo "$person2:: hello rahim"
+echo "$person1:: how are you doing?"
+echo "$person2:: i am good. how are you?"
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=ramesh #no space before and after equaal
+person2=suresh
+
+echo "$person1:: hi $person2"
+echo "$person2:: hello $person2"
+echo "$person1:: how are you doing?"
+echo "$person2:: i am good. how are you?"
