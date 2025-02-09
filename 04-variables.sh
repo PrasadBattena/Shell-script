c19de8998075b44b1e2c3551783bb3048352554c
prasad <prasadbattena@gmail.com>
2025-02-09 12:46:34 +0530

    converstaion added

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,6 @@ person1=$1
 person2=$2
 
 echo "$person1:: hi $person2"
-echo "$person2:: hello $person2"
+echo "$person2:: hello $person1"
 echo "$person1:: how are you doing?"
 echo "$person2:: i am good. how are you?"
