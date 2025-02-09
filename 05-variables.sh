98585d2ee1583e7d2759182827a1ec1a75114d43
prasad <prasadbattena@gmail.com>
2025-02-09 13:15:36 +0530

    user password

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,3 +2,5 @@
 echo "please enter your username::" #text entered here will be added to value
 read -s username
 echo "username entered: $username"
+echo "please enter your password::"
+read -s password
