997ddfd438653b73282cd498da7f994c25dd6901
prasad <prasadbattena@gmail.com>
2025-02-10 12:36:17 +0530

    user password

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+number=$1
+
+# -gt, -lt, -eq, -ge ,-le, gretarer than, le less than equal to 
+if [$number -gt 100]
+then
+    echo "given number is greater than 100"
+else   
+    echo "given number is lesss tha or equal to 100"
+fi
