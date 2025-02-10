f5e72e1599059c1b0a89036e30878f07743d957d
prasad <prasadbattena@gmail.com>
2025-02-10 13:24:58 +0530

    user password

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,22 @@ then
 
  dnf install mysql -y
 
+ if [$? -ne 0 ]
+
+ then
+     echo "installing mysql....Failure"
+     exit 1
+  else 
+       echo "installing mysql...Sucss"
+
+    fi
+
+
  dnf install git -y
+ if [$? -ne 0 ]
+ then 
+    echo "insatlling git... Failure"
+    exit 1
+ else
+    echo "installing git....success"
+ fi      
