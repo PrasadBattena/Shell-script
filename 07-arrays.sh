0149989d2bc74519fbdb5aa064049fe0fe115452
prasad <prasadbattena@gmail.com>
2025-02-10 11:17:13 +0530

    user password

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+movie=("pushpa" "rrr" "devara")
+# index starts from 0, size is 3
+
+echo "first movie: ${movies[0]}"
+echo "second movie: ${movies[1]}"
+echo "third movie: ${movies[2]}"
+
+echo "all movies are: ${movies[@]}"
