3def16dbae8cd0980f963e15067da7a7dc45d4ab
prasad <prasadbattena@gmail.com>
2025-02-25 14:55:19 +0530

    Scripts added

---
+++ 13-logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+
+validate(){
+
+    if [ $1 -ne 0 ]
+    then 
+      echo -e "$2...$R Failure"
+      exit 1
+     else
+        echo -e  "$2...$G sucsess"
+      fi   
+}
+
+if [ $USERID -ne 0 ]
+then 
+   echo "Error:: You must have sudo access to execute this script"
+   exit 1 #other than 0
+ fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] 
+then #not installed
+    dnf install mysql -y
+    validate $? "Installing mysql"
+
+    else
+     echo -e "mysql is already...$Y installed"
+fi
+
+
+ dnf list installed git
+
+ if [ $? -ne 0 ]
+ then 
+    dnf install git -y
+   validate $? "Installing Git"
+else
+   echo -e "Git is already...$Y Installed"
+ fi   
