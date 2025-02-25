d46cf6cd086183bfcb975999311df4d75447486d
prasad <prasadbattena@gmail.com>
2025-02-25 15:46:30 +0530

    Scripts added

--- 13-logs.sh
+++ 13-logs.sh
@@ -5,16 +5,17 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%s)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 validate(){
 
     if [ $1 -ne 0 ]
     then 
-      echo -e "$2...$R Failure"
+      echo -e "$2...$R Failure $N"
       exit 1
      else
         echo -e  "$2...$G sucsess"
