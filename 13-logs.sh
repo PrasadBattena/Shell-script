33033d8b36828eeb6439de07b9fa341cb1ee465f
prasad <prasadbattena@gmail.com>
2025-02-25 15:37:09 +0530

    Scripts added

--- 13-logs.sh
+++ 13-logs.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%y-%m-%d-%h-%m-%s)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%s)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 validate(){
