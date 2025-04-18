92f04a8e72b22681c803ce8c11b62c1965f13cb0
prasad <prasadbattena@gmail.com>
2025-04-18 19:51:58 +0530

    shell-script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-number=$1
+
+NUMBER=$1
 
 # -gt, -lt, -eq, -ge ,-le, gretarer than, le less than equal to 
-if [ $number -gt 100 ]
+
+if [ $NUMBER -gt 100 ]
 then
-    echo "given number is greater than 100"
+    echo "Given number is greater than 100"
 else   
-    echo "given number is lesss tha or equal to 100"
+    echo "Given number is lesss tha or equal to 100"
 fi
