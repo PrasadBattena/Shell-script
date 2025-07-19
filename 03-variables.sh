464323836494d05bb42c1c4d22f7f409891e21a7
prasad <prasadbattena@gmail.com>
2025-07-19 19:59:50 +0530

    added

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,6 +3,6 @@ Person1=ramesh #no space before and after equaal
 Person2=suresh
 
 echo "$Person1:: Hi $Person2"
-echo "$Person2:: Hello $Person2"
+echo "$Person2:: Hello $Person1"
 echo "$Person1:: How are you doing?"
 echo "$Person2:: I am good. how are you?"
