62a56665f45b94444bbbd6acaa013a178c66ed7d
prasad <prasadbattena@gmail.com>
2025-07-19 19:43:27 +0530

    added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$person1:: hi robert"
-echo "$person2:: hello rahim"
-echo "$person1:: how are you doing?"
-echo "$person2:: i am good. how are you?"
+echo "$person1:: Hi Robert"
+echo "$person2:: Hello Rahim"
+echo "$person1:: How are you doing?"
+echo "$person2:: I am good. how are you?"
