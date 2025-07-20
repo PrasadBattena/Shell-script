43453d92a59beaecdb11c7537b2b34902af6f6b3
prasad <prasadbattena@gmail.com>
2025-07-20 10:50:20 +0530

    added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-movie=("pushpa" "rrr" "devara")
+Movies=("pushpa" "rrr" "devara")
 # index starts from 0, size is 3
 
-echo "First movie: ${movies[0]}"
-echo "Second movie: ${movies[1]}"
-echo "Third movie: ${movies[2]}"
+echo "First movie: ${Movies[0]}"
+echo "Second movie: ${Movies[1]}"
+echo "Third movie: ${Movies[2]}"
 
-echo "All movies are: ${movies[@]}"
+echo "All Movies are: ${Movies[@]}"
